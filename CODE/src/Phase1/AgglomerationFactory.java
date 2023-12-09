2f0f08e7e2a31639a55194d3243c2c4eecfc2dbe
fayeldegg <146241769+fayeldegg@users.noreply.github.com>
2023-12-09 18:14:19 +0100

    Update AgglomerationFactory.java

--- CODE/src/Phase1/AgglomerationFactory.java
+++ CODE/src/Phase1/AgglomerationFactory.java
@@ -1,17 +1,148 @@
 package Phase1;
-
-
+/** Permet toute la Gestion du déroulement du programme
+ * @author Fayel Degguiche
+ * @author Yanis Hammaci
+ * @author Yanis Allain
+ */
 import java.util.Scanner;
 
-import Phase1.VilleException.villeHasNotZoneRecharge;
-import Phase1.VilleException.villeHasZoneRecharge;
-import Phase1.VilleException.villeVoisinesHasNotZoneRecharge;
-
-import java.util.InputMismatchException;
-
 public class AgglomerationFactory {
+	
+	/** Permet de créer le menu général */
+	public void menuAgglomeration() {
+		Agglomeration agg = fabriqueAgglomeration();
+		
+		creationVille(agg);
+		menuCreationAgglomeration(agg);
+		
+		menuGestionAgglomeration(agg);
+		
+	}
+	
+	/**Permet de créer une agglomeration à partir du menu
+	 * @param agg l'agglomeration que l'on souhaite créer
+	 */
+	public void menuCreationAgglomeration(Agglomeration agg) {
+		boolean tourne = true;
+		
+		while(tourne) {
+			Scanner lectureClavier = new Scanner(System.in) ;
+			afficherMenuCreationAgglomeration();
+			int choix = lectureClavier.nextInt();
+			switch(choix) {
+				case 1: creationRoute(agg);
+						break;
+				case 2: System.out.println("L'agglomération a été créer");
+						
+						tourne = false;
+						break;	
+				default:System.out.println("Mauvais choix, réessayer");
+			}
+		}
+		agg.afficherVilleAvecZoneRecharge();
+		agg.afficherListeAdjacence();
+		
+	}
+	/**Permet de gérer une agglomeration à partir du Menu
+	 * @param agg l'agglomeration que l'on souhaite gérer
+	 */
+	public void menuGestionAgglomeration(Agglomeration agg)  {
+	
+	
+		boolean tourne = true;
+		
+		while(tourne) {
+			Scanner lectureClavier = new Scanner(System.in) ;
+			afficherMenuGestionAgglomeration();
+			int choix = lectureClavier.nextInt();
+			
+			switch(choix) {
+			
+			
+				case 1: String nomAAjouter = Saisie.lireChaine("Entrez le nom de la ville où vous voulez ajouter une Zone de Recharge:\n");
+				
+						Ville aAjouter = agg.rechercherVille(nomAAjouter);
+						agg.ajouterZoneRecharge(aAjouter);
 
-	// créer une agglomeration
+						break;
+				case 2: 
+						String nomARetirer = Saisie.lireChaine("Entrez le nom de la ville où vous voulez retirer une Zone de Recharge\n");
+						Ville aRetirer = agg.rechercherVille(nomARetirer);
+						agg.retirerZoneRecharge(aRetirer);
+						//agg.retirerVilleAvecZoneRecharge(aRetirer);
+						//agg.ajouterVilleSansZoneRecharge(aRetirer);
+						break;
+						
+				case 3: System.out.println("L'agglomération est terminé");
+						tourne = false;
+						break;	
+				default:System.out.println("Mauvais choix, réessayer");
+				
+			}
+			agg.afficherVilleAvecZoneRecharge();
+			agg.afficherListeAdjacence();
+		
+		}
+	
+	}
+	/**Permet de créer les villes de l'agglomeration
+	 * @param agg l'agglomeration dont on veux créer les villes
+	 */
+	public void creationVille(Agglomeration agg) {
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.println("Combien de ville contient l'agglomération ?");
+		int nbVille = lectureClavier.nextInt();
+		
+		for(int i = 0; i<nbVille ; i++) {
+			System.out.println("Entrez le nom de la ville n°"+(i+1)+":");
+			Ville v = fabriqueVille();
+			agg.ajouterVille(v);
+		}
+		agg.afficherVilleAvecZoneRecharge();
+	}
+	
+	/**Permet de créer les routes de l'agglomeration
+	 * @param agg l'agglomeration dont on veut créer les routes
+	 */
+	public void creationRoute(Agglomeration agg) {
+		String nomPremiereVille = Saisie.lireChaine("Entrez le nom de la premiere Ville : \n");
+		Ville premiereVille = agg.rechercherVille(nomPremiereVille);
+		
+		String nomDeuxiemeVille = Saisie.lireChaine("Entrez le nom de la deuxième Ville : \n");
+		Ville deuxiemeVille = agg.rechercherVille(nomDeuxiemeVille);
+		agg.ajouterRoute(premiereVille, deuxiemeVille);
+		//System.out.println("La route entre " + premiereVille + " et " + deuxiemeVille + " a été créer");
+		//System.out.println(premiereVille + " - " + deuxiemeVille);
+	}
+	
+	/** Instancie une Agglomeration */
+	public Agglomeration fabriqueAgglomeration() {
+		return new Agglomeration();
+	}
+	
+	
+	/** Instancie une ville */
+	public Ville fabriqueVille() {
+		Scanner lectureClavier = new Scanner(System.in);
+		//System.out.println("Entrez le nom de la ville : ") ;
+		String nom = lectureClavier.nextLine();
+		return new Ville(nom) ;
+	}
+	
+	public void afficherMenuCreationAgglomeration() {
+		System.out.println("1 : Ajouter une route");
+		System.out.println("2 : Fin");
+	}
+	
+	public void afficherMenuGestionAgglomeration() {
+		System.out.println("\n1 : Ajouter une zone de recharge");
+		System.out.println("2 : Retirer une zone de recharge");
+		System.out.println("3 : Fin");
+	}
+	
+	/*
+	 * 
+	 * // créer une agglomeration
 	public static Agglomeration fabriqueAgglomeration() {
 		return new Agglomeration();
 	}
@@ -34,17 +165,14 @@ public class AgglomerationFactory {
 	// menu pour créer une agglomeration
 	public static void menuCreationAgglomeration(Agglomeration agg) {
 		boolean tourne = true;
-		
+		Scanner lectureClavier = new Scanner(System.in);
 
 		while (tourne) {
 
 			
 			try {
-				
-				afficherMenuCreationAgglomeration();
-				Scanner lectureClavier = new Scanner(System.in);
 				int choix = lectureClavier.nextInt();
-
+				afficherMenuCreationAgglomeration();
 				switch (choix) {
 					case 1:
 						try {
@@ -153,5 +281,7 @@ public class AgglomerationFactory {
 		System.out.println(agg.toString());
 		menuGestionAgglomeration(agg); // ajouter les zones de recharges
 	}
+
+	 */
 
 }
