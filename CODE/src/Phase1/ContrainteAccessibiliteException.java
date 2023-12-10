7d259fd210149afec7227b66c9630b15b8b5de29
fayeldegg <146241769+fayeldegg@users.noreply.github.com>
2023-12-10 14:44:06 +0100

    Add files via upload

---
+++ CODE/src/Phase1/ContrainteAccessibiliteException.java
@@ -0,0 +1,18 @@
+package ProjetPAA;
+
+/** Cette classe permet de gérer l'exception de la 
+ * contrainte d'accessibilité
+ * @author Fayel Degguiche
+ * @author Yanis Hammaci
+ * @author Yanis Allain
+ *
+ */
+public class ContrainteAccessibiliteException extends AgglomerationException{
+	
+	
+	private static final long serialVersionUID = -7216021069771571491L;
+
+	public ContrainteAccessibiliteException() {
+		super("Retirer cette ville enfreint la contrainte d'accessibilité");
+	}
+}
